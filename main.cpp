8cfa62a1a39248586c1cfb475f0b372bb850c7aa
Игорь <@DESKTOP-URMNCOP>
2023-02-06 12:46:52 +0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+class IVehicle {
+public:
+	virtual void printName() const = 0;
+	virtual ~IVehicle() {}
+};
+class Car : public IVehicle {
+public:
+	void printName() const override {
+		std::cout << "Car class" << std::endl;
+	}
+};
+void foo(const std::vector<IVehicle*>& vec) { 
+//void foo(const std::vector<const IVehicle*>& vec) убрали const
+	for (size_t i = 0; i < vec.size(); i++) {
+		vec[i]->printName();
+	}
+}
+int main() {
+	IVehicle* vehicle = new Car(); 
+	//IVehicle* vehicle = new IVehicle();
+	IVehicle* vehicle2 = new Car();
+	std::vector<IVehicle*> vec{ vehicle, vehicle2 };
+	//std::vector<IVehicle*> vec{ vehicle2 };
+	foo(vec);
+}
